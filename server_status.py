b553fb52c47ba4ff49709afafa679c22ac6233d3
CalaMiTY0311 <axelsilas12@naver.com>
2023-06-27 20:01:38 +0900

    add cm_server status but cs_server status print {} <- null so need fix

--- server_status.py
+++ server_status.py
@@ -8,39 +8,78 @@ def parse_page(driver):
     # BeautifulSoup을 사용하여 페이지 파싱
     soup = BeautifulSoup(page_source, "html.parser")
 
-    cs_country_id = ['south_korea', 'japan', 'hong_kong', 'china_shanghai']
+    cm_country_id = [
+                     'seo', 
+                     'tyo', 
+                     'hkg', 
+                     'gua', 
+                     'sha', 
+                     'tia'
+                     ]
+    
+    cm_class_check = ['good', 'muted']
+    cm_class_base = 'status '
+    #cm_NA_checker = 'No servers in this location'
+    #cm_status_element = soup.find("span", class_=cm_class_base + cm_class_check[0], id=cm_country_id[0])
+    cm_server_statuses = {}
+    #print(cm_status_element)
+
+    for country_id in cm_country_id:
+        cm_status_element = None
+
+        for cm_class in cm_class_check:
+            check = soup.find("span",
+                              class_=cm_class_base + cm_class,
+                              id=country_id)
+            if check is not None:
+                cm_status_element = check.text.strip()
+                break
+
+        if cm_status_element is not None:
+            cm_server_statuses[country_id] = cm_status_element
+        else:
+            cm_server_statuses[country_id] = "N/A"
+#____________________________________________________________________
+
+    # if cm_status_element is not None:
+    #     cm_status_element = cm_status_element.text.strip()
+    #     print(cm_status_element)
+    # else:
+    #     print("N/A")
+    
+#--------------------------------------cs server-----------------------------------------------
+    cs_country_id = [
+                     'south_korea', 
+                     'japan', 
+                     'hong_kong', 
+                     'china_guangzhou', 
+                     'china_shanghai', 
+                     'china_tianjin'
+                     ]
+    
     cs_country_id_base = 'csgo_'
     cs_class_check = ['good', 'minor']
     cs_class_base = 'status '
-    server_statuses = {}  # 서버 상태를 저장할 딕셔너리
-    #status_element = soup.find("span", class_="status minor", id="csgo_japan")
+    cs_server_statuses = {}  # 서버 상태를 저장할 딕셔너리
+    #cs_status_element = soup.find("span", class_="status minor", id="csgo_japan")
 
-    status_element = None
+    cs_status_element = None
 
     for country_id in cs_country_id:
-        status_element = None
+        cs_status_element = None
 
         for cs_class in cs_class_check:
             check = soup.find("span",
                               class_=cs_class_base + cs_class,
                               id=cs_country_id_base + country_id)
             if check is not None:
-                status_element = check.text.strip()
+                cs_status_element = check.text.strip()
                 break
 
-        if status_element is not None:
-            server_statuses[country_id] = status_element
-
-    return server_statuses
-
-    #print(status_element)
-    if status_element is not None:
-        # 요소의 텍스트 가져오기
-        status_text = status_element
-        #print(status_text)
-        return status_text
-
-    #return None
+        if cs_status_element is not None:
+            cs_server_statuses[country_id] = cs_status_element
+#--------------------------------------cs server-----------------------------------------------
+    return cs_server_statuses, cm_server_statuses
 
 # Selenium 웹 드라이버 설정
 driver = webdriver.Chrome()  # Chrome 드라이버 사용

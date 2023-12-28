4864c2417251d123d83ba2fa54b533dbfc7b9158
CalaMiTY0311 <axelsilas12@naver.com>
2023-12-28 16:59:06 +0900

    status done

--- status/server_status.py
+++ status/server_status.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -17,15 +16,18 @@ def status_parser():
 
     driver.get(url)
 
-    wait = WebDriverWait(driver, 5)
-    time.sleep(2)
+    wait = WebDriverWait(driver, 2)
+    time.sleep(1)
 
 
     elements = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "regions")))
 
-    status={}
-    for element in elements:
+    result = {}
+    regions_list = ['Europe','Americas','Asia','Rest of world']
+
+    for region, element in zip(regions_list, elements):
         lines = element.text.split('\n')
+        status={}
         for i in range(0, len(lines), 3):
 
             regions = list(lines[i].split(' '))
@@ -34,8 +36,11 @@ def status_parser():
 
             status_values = lines[i + 1:i + 3]
             status[regions_code] = {regions_name: status_values}
-
-    return status
+        
+        result[region] = status
+        
+            
+    return result
 
 from pprint import pprint
 pprint(status_parser())

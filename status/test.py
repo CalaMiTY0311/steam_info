52ced61f0eeaaabf700fb9040198dd107e8c8f61
CalaMiTY0311 <axelsilas12@naver.com>
2023-12-28 15:38:06 +0900

    parsing done

--- status/test.py
+++ status/test.py
@@ -20,21 +20,22 @@ def status_parser():
     wait = WebDriverWait(driver, 5)
     time.sleep(2)
 
-    status = {}
 
     elements = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "regions")))
+
+    status={}
     for element in elements:
         lines = element.text.split('\n')
-        for line in lines:
-            print(line)
+        for i in range(0, len(lines), 3):
 
+            regions = list(lines[i].split(' '))
+            regions_code = regions[0]
+            regions_name = ' '.join(regions[1:])
 
+            status_values = lines[i + 1:i + 3]
+            status[regions_code] = {regions_name: status_values}
 
-
-
-        
-
-    # return lst
+    return status
 
 from pprint import pprint
-print(status_parser())
+pprint(status_parser())

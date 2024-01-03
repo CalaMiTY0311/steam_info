fb6fe9ed670826bf7dd55beb094a55d5524c82ce
CalaMiTY0311 <axelsilas12@naver.com>
2024-01-03 12:21:27 +0900

    upcomming sales index add

--- event_parsing/elements.py
+++ event_parsing/elements.py
@@ -5,25 +5,47 @@ from datetime import datetime
 import pytz
 
 wait = on_chrome()
-# now_sale_check = wait.until(EC.presence_of_all_elements_located((By.ID, "hero")))
 
-# for now_sale in now_sale_check:
-#     YMD = now_sale.find_elements(By.TAG_NAME, "strong")
-#     YMD = YMD[-1].text
+now_sale_check = wait.until(EC.presence_of_all_elements_located((By.ID, "hero")))
 
-#     HM = now_sale.find_elements(By.XPATH, "//span[@class='font-light text-slate-400']")
-#     HM=HM[0].text
-#     HM = list(HM.split(' '))
-#     HM = [HM[0].replace('(', ''),HM[1]]
+for now_sale in now_sale_check:
+    YMD = now_sale.find_elements(By.TAG_NAME, "strong")
+    YMD = YMD[-1].text
 
-# combined_datetime_string = f"{YMD} {HM[0]} {HM[1]}"
-# combined_datetime = datetime.strptime(combined_datetime_string, "%m/%d/%Y %I:%M %p")
+    HM = now_sale.find_elements(By.XPATH, "//span[@class='font-light text-slate-400']")
+    HM=HM[0].text
+    HM = list(HM.split(' '))
+    HM = [HM[0].replace('(', ''),HM[1]]
 
-# input_timezone = pytz.timezone('America/Los_Angeles')
-# combined_datetime = input_timezone.localize(combined_datetime)
-# output_timezone = pytz.timezone('Asia/Seoul')
-# korean_datetime = combined_datetime.astimezone(output_timezone)
+combined_datetime_string = f"{YMD} {HM[0]} {HM[1]}"
+combined_datetime = datetime.strptime(combined_datetime_string, "%m/%d/%Y %I:%M %p")
 
-upcoming_sale_check = wait.until(EC.presence_of_all_elements_located((By.ID, "upcoming-sales")))
-for i in upcoming_sale_check:
-    print(i.text)
+input_timezone = pytz.timezone('America/Los_Angeles')
+combined_datetime = input_timezone.localize(combined_datetime)
+output_timezone = pytz.timezone('Asia/Seoul')
+korean_datetime = combined_datetime.astimezone(output_timezone)
+
+print(korean_datetime)
+
+upcoming_sales_check = wait.until(EC.presence_of_all_elements_located((By.ID, "upcoming-sales")))
+
+upcoming_sales_info=[]
+for i in upcoming_sales_check:
+    lines = i.text.split('\n')
+    for line in lines:
+        upcoming_sales_info.append(line)
+upcoming_sales_info = upcoming_sales_info[2:]
+upcoming_sales_info = [upcoming_sales_info[i:i+4] for i in range(0, len(upcoming_sales_info), 5)]
+
+result = []
+for lst in upcoming_sales_info:
+    result_dict = {
+        "event_name": lst[0],
+        "event_quality": lst[1],
+        "event_start": lst[2],
+        "event_end": lst[3],
+
+    }
+    result.append(result_dict)
+    
+
